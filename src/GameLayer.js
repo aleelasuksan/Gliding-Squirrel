332f79ada70353facb7f6d8485b19b67ed1ec4d3
grief.d.lament@gmail.com <grief.d.lament@gmail.com>
2014-03-10 19:11:10 +0700

    player can jump (no physics)

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -2,12 +2,20 @@ var GameLayer = cc.LayerColor.extend({
     init: function() {
         this._super( new cc.Color4B( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
-		this.player = new Player();
-		this.player.setPosition( cc.p( 400, 300 ) );
+		this.player = new Player( 400, 100 );
 		this.addChild(this.player);
+		this.player.scheduleUpdate();
 		console.log('finish init gamelayer');
+		this.setKeyboardEnabled( true );
+		//this.scheduleUpdate();
         return true;
-    }
+    },
+	
+	onKeyDown: function( e ) {
+		if( ( e == cc.KEY.up || e == cc.KEY.space ) && !this.player.isJump ) {
+			this.player.jump();
+		}
+	}
 });
 
 var StartScene = cc.Scene.extend({
--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,32 @@
 var Player = cc.Sprite.extend({
-	ctor: function() {
+	ctor: function( x, y ) {
 		this._super();
 		this.initWithFile( 'res/images/player.png' );
 		this.setAnchorPoint( cc.p( 0.5, 0.5 ) );
+		this.x = x;
+		this.y = y;
+		this.v = 30;
+		this.g = -1;
+		this.isJump = false;
+		this.updatePosition();
 		console.log('init player');
+	},
+	
+	update: function( dt ) {
+		if(this.isJump) {
+			console.log('jump');
+			this.y += this.v;
+			this.updatePosition();
+			this.v += this.g;
+		}
+	},
+	
+	jump: function() {
+		this.isJump = true;
+	},
+	
+	updatePosition: function() {
+		this.setPosition( cc.p( this.x, this.y ) );
 	}
+	
 });
